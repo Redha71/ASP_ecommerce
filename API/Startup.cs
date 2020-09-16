37cf486cd2fb41e79f7c609c7e73ef5a5f17f750
Redha71 <redhakaban@hotmail.com>
2020-09-16 21:49:28 +0100

    end 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,14 @@ namespace API
              X.UseSqlite(_config.GetConnectionString("DefaultConnection")));
              services.AddApplicationServices();
              services.AddSwaggerDocumentation();
+             services.AddCors(opt =>
+             {
+                 opt.AddPolicy("CorsPolicy", policy =>
+                 {
+                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                 });
+
+             });
 
 
         }
@@ -44,6 +52,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
             app.UseSwaggerDocunention();
